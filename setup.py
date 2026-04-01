38f9c51df1136e731aff5eef932c5a0adb7bcee5
Harshit <harshituohyd@gmail.com>
2026-04-01 13:47:01 +0530

    completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="llm-apps-with-chainlit",
+    version="0.0.0",
+    author="Harshit",
+    packages=find_packages(),
+    install_requires=[]
+)
